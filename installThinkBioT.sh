17c5a4cb05347ea5b96a2653ae8626185a53bcbf
mFitzy <maritafitzgerald@gmail.com>
2019-08-31 03:56:26 +1000

    edit install

--- installThinkBioT.sh
+++ installThinkBioT.sh
@@ -1,4 +1,4 @@
-[ -z $BASH ] && { exec bash "$0" "$@" || exit; } 
+[ -z $BASH ] && { exec bash "$0" "$@" || exit; }
 #!/bin/bash
 # file: installThinkBioT.sh
 # sudo wget -O installThinkBioT.sh https://github.com/mefitzgerald/ThinkBioT/raw/master/installThinkBioT.sh
@@ -31,116 +31,103 @@ if [ "$(id -u)" != 0 ]; then
   exit 1
 fi
 
+# target directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/ThinkBioT"
+
 # error counter
 ERR=0
 
-echo '=============================================================================='
-echo '|                                                                            |'
-echo '|             ThinkBioT Software Installation Script                         |'
-echo '|                                                                            |'
-echo '=============================================================================='
-echo '    ThinkBioT  Copyright (C) 2019  Marita Fitzgerald '
-echo '    This program comes with ABSOLUTELY NO WARRANTY;  '
-echo '    This is free software, and you are welcome to redistribute it '
-echo '    under certain conditions, For more information view the License at ; '
-echo '    https://github.com/mefitzgerald/ThinkBioT/blob/master/LICENSE'
+echo '================================================================================'
+echo '|                                                                              |'
+echo '|                   ThinkBioT Software Installation Script                     |'
+echo '|                                                                              |'
+echo '================================================================================'
 
-
-# install ThinkBioT
+# install ThinkBioT test
 if [ $ERR -eq 0 ]; then
   echo '>>> Install ThinkBioT'
-  #check if folder already exists
-  if [ -d "ThinkBioT" ]; then
+  if [ -d "ThinkBioT2" ]; then
     echo 'ThinkBioT is installed already, skip this step.'
   else
-	echo '>>> Begin ThinkBioT Installation'
-	#ensure installtion in home directory
-	cd ~
-	mkdir ThinkBioT	
-	# Install rasbian packages
-	echo '>>> Install dependencies'
-	PACKAGES="git sox sqlite3 python-scipy=0.18.1-2 python-numpy=1:1.12.1-3 python-matplotlib git screen usbutils"
-	apt-get install $PACKAGES -y || ((ERR++))
-	
-	# Install Python packages
-	pip install pyyaml==5.1.1
-	pip install pyserial
-	
-	# get RockBlock files
-	git clone https://github.com/MakerSnake/pyRockBlock || ((ERR++))
-	
-	# get ThinkBioT install files
-	wget https://github.com/mefitzgerald/ThinkBioT/raw/master/ThinkBioT/tbtzip.zip -O tbtzip.zip || ((ERR++))
+    echo '>>> Making Directory'
+    mkdir ThinkBioT
+   
+    echo '>>> Install dependencies'
+    # Install rasbian packages
+    PACKAGES="git sox sqlite3 python-scipy=0.18.1-2 python-numpy=1:1.12.1-3 python-matplotlib git screen usbutils"
+    apt-get install $PACKAGES -y || ((ERR++))  
+  
+    # Install Python packages
+    pip install pyyaml==5.1.1 || ((ERR++))
+    pip install pyserial || ((ERR++))
+  
+    # get RockBlock files
+    git clone https://github.com/MakerSnake/pyRockBlock || ((ERR++))
+  
+    # get ThinkBioT install files
+    wget https://github.com/mefitzgerald/ThinkBioT/raw/master/ThinkBioT/tbtzip.zip -O tbtzip.zip || ((ERR++))
     # unzip files
-	unzip tbtzip.zip -d ThinkBioT || ((ERR++))
-	# move to directory files were unzipped to 
+    unzip tbtzip.zip -d ThinkBioT || ((ERR++))
+    # move to directory files were unzipped to 
     cd ThinkBioT
-	# make files executable
-	chmod +x tbt_Start.py || ((ERR++))
-	chmod +x tbt_DB_Ini.py	|| ((ERR++))
-	chmod +x trecord.sh || ((ERR++))
-	chmod +x arecord.sh || ((ERR++))
-	chmod +x transmit.sh || ((ERR++))
-	chmod +x classify_spect.sh || ((ERR++))
-	
-	echo '>>> Set soundBlaster as default audio device'
-	# move .asoundrc audio settings file to home
-	mv -i .asoundrc ~  || ((ERR++))
-	# move asound configuration file
-	mv -i asound.conf /etc/asound.conf || ((ERR++))
-	
-	# move service unit file to systemd file (-i confirms before over writing)
-	mv -i tbt.service /lib/systemd/system/tbt.service || ((ERR++))
-	echo '>>> Enable start up service'
-	# enable service to be started next boot
-	systemctl enable tbt.service || ((ERR++))
-	
-	#setup sqlite database
-	echo '>>> Create ThinkBioT database'
-	python3 tbt_DB_Ini.py
-	sleep 1
-	rm tbt_DB_Ini.py
-	rm tbt_dbSchema.sql
-	
-	echo '>>> Create Index calculation files'
-	# move files for index calculation
-	cd ~/ThinkBioT
-	# create required directories
-	mkdir ~/ThinkBioT/AcousticIndices
-
-	# populate AcousticIndices
-	mv -i arecord.sh ~/ThinkBioT/AcousticIndices/arecord.sh || ((ERR++))
-	mv -i acoustic_index.py ~/ThinkBioT/AcousticIndices/acoustic_index.py || ((ERR++))
-	mv -i compute_indice.py ~/ThinkBioT/AcousticIndices/compute_indice.py || ((ERR++))
-	mv -i tbt_indexprocess.py ~/ThinkBioT/AcousticIndices/tbt_indexprocess.py || ((ERR++))
-	mv -i AI_README.md ~/ThinkBioT/AcousticIndices/AI_README.md || ((ERR++))
-	mv -i AI_LICENSE.txt ~/ThinkBioT/AcousticIndices/AI_LICENSE.txt || ((ERR++))
-	mv -i __init__.py ~/ThinkBioT/AcousticIndices/__init__.py || ((ERR++))
-	# set executable
-	chmod +x ~/ThinkBioT/AcousticIndices/tbt_indexprocess.py || ((ERR++))
-	# create required directories
-	mkdir ~/ThinkBioT/AcousticIndices/yaml
-	#populate yaml/tbt_index.yaml
-	mv -i tbt_index.yaml ~/ThinkBioT/AcousticIndices/yaml/tbt_index.yaml || ((ERR++))
-	mkdir ~/ThinkBioT/AcousticIndices/IAudioIn
-	
-	echo '>>> Create Classification calculation files'
-	# move files for classification calculation
-	cd ~/ThinkBioT
-	mkdir ~/ThinkBioT/ClassProcess
-	mkdir ~/ThinkBioT/ClassProcess/CAudioIn	
-	mkdir ~/ThinkBioT/ClassProcess/CModel
-	mkdir ~/ThinkBioT/ClassProcess/CSpectrograms
-	mv -i trecord.sh ~/ThinkBioT/ClassProcess/trecord.sh || ((ERR++))
-	mv -i classify_spect.py ~/ThinkBioT/ClassProcess/CModel.sh/classify_spect.py || ((ERR++))
-	cd ~	
-	# set ownership of ThinkBioT to pi
-	cd ~
-	chown -R pi:pi ThinkBioT
+  
+    # make files executable
+    chmod +x tbt_Start.py || ((ERR++))
+    chmod +x tbt_DB_Ini.py	|| ((ERR++))
+    chmod +x transmit.sh || ((ERR++))
+    
+    echo '>>> Set soundBlaster as default audio device'
+    # move .asoundrc audio settings file to home
+    mv -i .asoundrc ~  || ((ERR++))
+    # move asound configuration file
+    mv -i asound.conf /etc/asound.conf || ((ERR++))
+    
+    # move service unit file to systemd file (-i confirms before over writing)
+    mv -i tbt.service /lib/systemd/system/tbt.service || ((ERR++))
+    echo '>>> Enable start up service'
+    # enable service to be started next boot
+    systemctl enable tbt.service || ((ERR++))
+  
+    #setup sqlite database
+    echo '>>> Create ThinkBioT database'
+    python3 tbt_DB_Ini.py
+    sleep 1
+    rm tbt_DB_Ini.py
+    rm tbt_dbSchema.sql
+    
+    echo '>>> Create Index calculation files'  
+    mkdir AcousticIndices
+    # populate AcousticIndices
+    mv -i arecord.sh ./AcousticIndices/arecord.sh || ((ERR++))
+    mv -i acoustic_index.py ./AcousticIndices/acoustic_index.py || ((ERR++))
+    mv -i compute_indice.py ./AcousticIndices/compute_indice.py || ((ERR++))
+    mv -i tbt_indexprocess.py ./AcousticIndices/tbt_indexprocess.py || ((ERR++))
+    mv -i AI_README.md ./AcousticIndices/AI_README.md || ((ERR++))
+    mv -i AI_LICENSE.txt ./AcousticIndices/AI_LICENSE.txt || ((ERR++))
+    mv -i __init__.py ./AcousticIndices/__init__.py || ((ERR++))
+    # set executable
+    chmod +x ./AcousticIndices/tbt_indexprocess.py || ((ERR++))
+    chmod +x ./AcousticIndices/arecord.sh || ((ERR++))
+    # create required directories
+    mkdir ./AcousticIndices/yaml
+    #populate yaml/tbt_index.yaml
+    mv -i tbt_index.yaml ./AcousticIndices/yaml/tbt_index.yaml || ((ERR++))
+    mkdir ./AcousticIndices/IAudioIn
+    
+    echo '>>> Create Classification calculation files'
+    # move files for classification calculation
+    mkdir ./ClassProcess
+    mkdir ./ClassProcess/CAudioIn	
+    mkdir ./ClassProcess/CModel
+    mkdir ./ClassProcess/CSpectrograms
+    mv -i trecord.sh ./ClassProcess/trecord.sh || ((ERR++))
+    mv -i classify_spect.py ./ClassProcess/CModel.sh/classify_spect.py || ((ERR++))
+    chmod +x ./ClassProcess/trecord.sh || ((ERR++))
+    chmod +x ./ClassProcess/CModel.sh/classify_spect.py || ((ERR++))
+	cd ..
+    chown -R pi:pi ThinkBioT
     sleep 2
-	# delete redundant zip file
-	rm tbtzip.zip
-	
+    rm tbtzip.zip
   fi
 fi
 
@@ -150,3 +137,4 @@ if [ $ERR -eq 0 ]; then
 else
   echo '>>> Something went wrong. Please check the messages above :-('
 fi
+
